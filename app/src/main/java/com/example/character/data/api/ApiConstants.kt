201c5da8a0eb15418c1833d5482be3df8da9e782
Ezra Kanake <10kanake@gmail.com>
2022-04-17 11:24:57 +0300

    Update ApiConstants.kt
    change from http to https to get successfully fetch data

--- app/src/main/java/com/example/character/data/api/ApiConstants.kt
+++ app/src/main/java/com/example/character/data/api/ApiConstants.kt
@@ -1,6 +1,6 @@
 package com.example.character.data.api
 
 object ApiConstants {
-    const val BASE_URL = "http://hp-api.herokuapp.com/api/"
+    const val BASE_URL = "https://hp-api.herokuapp.com/api/"
     const val END_POINTS = "characters"
 }
